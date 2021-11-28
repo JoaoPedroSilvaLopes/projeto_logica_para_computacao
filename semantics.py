84a8a40bc5402af4a75f6729d3e4599d4672b82b
finkl <finkl1@hotmail.com>
2021-11-28 00:55:45 -0300

    Implementação das funções satisfiability_brute_force, sat e remove_atoms

--- semantics.py
+++ semantics.py
@@ -1,10 +1,8 @@
-"""The goal in this module is to define functions associated with the semantics of formulas in propositional logic. """
-
-
 from formula import *
 from functions import atoms
 
-   
+
+# VERIFICAR O VALOR VERDADE DA FORMULA COM TAL INTERPRETAÇÃO  
 def truth_value(formula, interpretation):
     
     if isinstance(formula, Atom): # Verificar se a fórmula é uma atomica
@@ -38,170 +36,85 @@ def truth_value(formula, interpretation):
             return False
 
 
-def is_logical_consequence(premises, conclusion):  # function TT-Entails? in the book AIMA.
-    """Returns True if the conclusion is a logical consequence of the set of premises. Otherwise, it returns False."""
-    pass
-    # ======== YOUR CODE HERE ========
-
-
-def is_logical_equivalence(formula1, formula2):
-    """Checks whether formula1 and formula2 are logically equivalent."""
-    pass
-    # ======== YOUR CODE HERE ========
-
-
-def is_valid(formula):
-    """Returns True if formula is a logically valid (tautology). Otherwise, it returns False"""
-    pass
-    # ======== YOUR CODE HERE ========
-
-
+# VERIFICAR A SATISFABILIDADE DE FORMA BRUTA
 def satisfiability_brute_force(formula):
 
     list_atoms = atoms(formula) # Lista das atomicas da formula
+    interpretation_parcial = []
+    vetorA = []
+                    
+    for interpretacao in interpretation_parcial:
+        vetorA.append(interpretacao[0])
     
-    print(list_atoms)
-    
-    # Interpretação
-    # DICA DE MELHORIA, QUEBRAR AS FORMULA E TENTAR DEDUZIR AS INTERPRETAÇÕES
-    #interpretation = {}
-    
-    # Retorno da função
-    # REMOVER AS ATOMICAS QUE JA RECEBERAM UMA INTERPRETAÇÃO PARCIAL DA LIST_ATOMS
-    return list_atoms #False #sat(formula, list_atoms, interpretation)
+    list_atoms = remove_atoms(list_atoms, vetorA)
+
+    return sat(formula, list_atoms, interpretation_parcial)
 
 
-# VERIFICAR AS POSSIBILIDADES DE SATISFABILIDADE 
-def sat(formula, list_atoms, interpretation):
-    # Verificar se list_atoms é um conjunto vazio
-    if list_atoms == {}:
-        # Caso o valor da formula seja verdadeiro
-        if truth_value(formula, interpretation):       
-            # Retornar a interpretação
-            return interpretation
+# VERIFICAR AS POSSIBILIDADES DE SATISFABILIDADE
+def sat(formula, list_atoms, interpretation_parcial):
+
+    copia_list_atoms = list_atoms.copy() # copia da list atoms para ser alterada na chamada recursiva
+    
+    if list_atoms == []: # se list_atoms é um conjunto vazio
+        if truth_value(formula, interpretation_parcial): # Caso o valor da interpretação seja verdadeiro
+            lista_true = [] # lista auxiliar para coletar somente os valores true
+            for array in interpretation_parcial: # for para percorrer a interpretação
+                if array[-1] == True: # se o ultimo valor da linha for True
+                    lista_true.append(array[0]) # acrescentar o primeiro valor à lista_true
+
+            return lista_true # Retornar a interpretação so com os valores verdadeiros
         
-        # Caso o valor seja falso
+        else: # Caso o valor da interpretação seja falso       
+            return False # Retornar False
+    
+    atomica_removida = copia_list_atoms.pop() # remoção do ultimo elemento da copia da list_atoms
+    
+    interpretacao1 = interpretation_parcial + [[atomica_removida, True]] # interpretação1 da atomica removida com a valoracao True
+    interpretacao2 = interpretation_parcial + [[atomica_removida, False]] # interpretação2 da atomica removida com a valoracao False
+    
+    resultado = sat(formula, copia_list_atoms, interpretacao1) # chamada recursiva para a primeira interpretação
+    
+    if resultado != False: # caso a chamada recursiva da primeira interpretação seja diferente de False
+        return resultado # retornar resultado
+    
+    else: # se não
+        resultado1 = sat(formula, copia_list_atoms, interpretacao2) # chamada recursiva para a segunda interpretação
+        
+        if resultado1 != False: # caso a chamada recursiva da segunda interpretação seja diferente de False
+            return resultado1 # retornar resultado1
+        
         else:
-            # Retornar False
-            return False
-    
-    # Remover elemento do conjunto e atribui-lo a variavel atom
-    atom = list_atoms.pop()
-    
-    # Cópia de atom
-    atom2 = atom
-    
-    # Adicionar as duas possíveis interpretações: True ou False
-    interpretation1 = {interpretation}.union(atom, True)
-    interpretation2 = {interpretation}.union(atom, False)
-    
-    # Resultado da interpretação
-    result = sat(formula, atom, interpretation1)
-    
-    # Se o resultado for True
-    if result != False:
-        # Retornar o resultado
-        return result
-    
-    # Chamada recursiva da função sat interpretação 2 caso a interpretação 1 seja falsa
-    return sat(formula, atom2, interpretation2)
-    
-# VERIFICAR A VALIDADE
-def validity_checking(formula):
-    
-    if satisfiability_brute_force(Not(formula)) == False: # Se a fórmula não for safisfatível, logo a fórmula é válida
-        return True # A fórmula é válida
-    
-    else: # Caso seja True
-        return False # A fórmula não é válida
+            return False # retornar False
+            
 
-#---------------------------------------------------------------
-# TESTES
-# Função para converter todas os operadores das forulas em And utilizando os conceitos da equivalencia lógica
-
-def conversor(formula):
+"""def teste(formula, interpretation):
     
-    if isinstance(formula, Atom):
-        return formula
+    if isinstance(formula, Atom):       
+        interpretation.append([str(formula), True]) 
     
     elif isinstance(formula, Not):
         if isinstance(formula.inner, Atom):
-            return formula
-        
+            interpretation.append([str(formula.inner), False])
+            
         else:
-            formula = conversor(formula.inner)
-            return formula
-    
-    elif isinstance(formula, And):
-        formula = And(conversor(formula.left), conversor(formula.right))
-    
-    elif isinstance(formula, Or):
-        formula = Not(And(Not(conversor(formula.left)), Not(conversor(formula.right))))
+           teste(formula.inner, interpretation)
 
-    elif isinstance(formula, Implies):
-        formulaParcial = Or(Not(conversor(formula.left)), conversor(formula.right))
-        formula = conversor(formulaParcial)
-    
-    return formula
-
-def simplificador_de_not(formula):
-    
-    if isinstance(formula, Atom):
-        return formula
-    
-    elif isinstance(formula, Not):
-        if isinstance(formula.inner, Not):
-            formula = simplificador_de_not(formula.inner.inner)
-            return formula
-        
-        else:
-            formula = Not(simplificador_de_not(formula.inner))
-            return formula
-    
     elif isinstance(formula, And) or isinstance(formula, Or) or isinstance(formula, Implies):
-        formula.left = simplificador_de_not(formula.left)
-        formula.right = simplificador_de_not(formula.right)
-        
-    return formula
-
-"""formula1 = Implies(Atom('q'), Atom('r'))
-formula2 = Implies(Not(Atom('q')), Atom('r'))
-formula3 = Implies(Atom('q'), Not(Atom('r')))
-formula4 = Implies(Not(Atom('q')), Not(Atom('r')))
-
-formula5 = Or(Atom('p'), Implies(Atom('q'), Atom('r')))
-
-print(f'A fórmula {formula5} converteu para: {conversor(formula5)}')
-formula6 = conversor(formula5)
-print(f'A fórmula {formula6} foi simplificada para: {simplificador_de_not(formula6)}')"""
+        teste(formula.left, interpretation)
+        teste(formula.right, interpretation)
     
+    return sorted(list(interpretation))"""
+        
 
+# REMOVER ATOMICAS QUE JA POSSUEM INTERPRETAÇÃO PARCIAL DA LIST_ATOMS  
+def remove_atoms(list_atoms, interpretation_parcial):
+    
+    lista_atoms_removidos = []
+    
+    for linha in list_atoms: # percorrer os elementos da list_atoms
+        if str(linha) not in interpretation_parcial: # se o elemento não estiver na interpretation_parcial
+            lista_atoms_removidos.append(linha) # acrescentar elemento na lista_atoms_removidos
+              
+    return lista_atoms_removidos # retornar lista_atoms_removidos
 
-"""X_PI <= 42.09_1_p
-X_PI <= 42.09_1_n
-X_PI <= 42.09_1_s
-
-
-X_LA <= 39.63_1_p
-X_LA <= 39.63_1_n
-X_LA <= 39.63_1_s
-
-
---
-X_GS <= 37.89_1_n
-X_GS <= 37.89_1_s
-
-
-X_PI <= 42.09_2_p
-X_PI <= 42.09_2_n
-X_PI <= 42.09_2_s
-
-
-X_LA <= 39.63_2_p
-X_LA <= 39.63_2_n
-X_LA <= 39.63_2_s
-
-
---
-X_GS <= 37.89_2_n
-X_GS <= 37.89_2_s"""
